2489e26204c52f8fa5c944e541abdbe2a9edf394
nidhi192004 <thakrenidhi41@gmail.com>
2024-12-24 19:40:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for your message! I will get back to you soon.');
+    document.getElementById('contact-form').reset();
+  });
+  
